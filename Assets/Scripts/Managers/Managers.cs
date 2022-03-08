9eef814ac436114089441ef1f07fb7b2058b3cc4
MightyChipmunk <21qqq4@gmail.com>
2022-03-08 21:17:08 +0900

    매니저 생성

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+
+    static Managers s_Instance;
+    public static Managers Instance { get { Init(); return s_Instance; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (s_Instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_Instance = go.GetComponent<Managers>();
+        }
+    }
+}
