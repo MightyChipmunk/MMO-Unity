bdd617800fb005da24bbfdfc17723f4895cbcbef
MightyChipmunk <21qqq4@gmail.com>
2022-03-22 17:14:37 +0900

    UI 자동화 구현

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class UI_Button : UI_Base
+{
+    enum Buttons
+    {
+        PointButton,
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    enum GameObjects
+    {
+        TestObject,
+    }
+
+    enum Images
+    {
+        ItemIcon,
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
+
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;        
+        AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
+    }
+
+    int _score = 0;
+
+    public void OnButtonClicked(PointerEventData data)
+    {
+        _score++;
+        GetText((int)Texts.ScoreText).text = $"Á¡¼ö: {_score}";
+    }
+}
---
+++ Assets/Scripts/Utils/Extension.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class Extension
+{
+    public static void AddUIEvent(this GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+        UI_Base.AddUIEvent(go, action, type);
+    }
+}
